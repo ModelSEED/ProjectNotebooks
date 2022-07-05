bb71bb941b76dd4c912e1accd87cf6e193315437
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-07-05 11:48:27 -0400

    reorganization

--- CommunityModeling/CommFitting/setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from setuptools import setup # Extension
-from Cython.Build import cythonize
-# from Cython.Compiler import Options; Options.annotate = True
-
-setup(
-    ext_modules=cythonize("mscommfitting.pyx", annotate=True),
-    package_dir={'CommFitting': ''}, zip_safe=False)
